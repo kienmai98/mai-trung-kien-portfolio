d02cf778a7c98b115dff9a0c02eaa62f80f3fe9f
mai trung kien <hasongva@gmail.com>
2023-04-13 23:11:02 +0700

    add antd

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,9 +1,10 @@
-export function Index() {
+import { Button } from 'libs/uikit/src';
+
+export default function Index() {
   return (
     <div id="welcome">
-      <h1 className="bg-red-500">Hello there, Welcome web 👋</h1>
+      <h1 className="ml-2 bg-red-500 pr-1">Hello there, Welcome web 👋</h1>
+      <Button>Test</Button>
     </div>
   );
 }
-
-export default Index;
