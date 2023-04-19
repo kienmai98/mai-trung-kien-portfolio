6dc4ae9c578b21ada78da122d462074d426b018e
mai trung kien <hasongva@gmail.com>
2023-04-19 21:40:30 +0700

    add index page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,9 @@
 export default function LoginPage() {
   return (
     <div className="container mx-auto flex h-screen w-screen flex-col items-center justify-center">
-      <h1 className="text-2xl font-semibold tracking-tight">Login page</h1>
+      <h1 className="text-2xl font-semibold tracking-tight">
+        Welcome back Kien
+      </h1>
     </div>
   );
 }
