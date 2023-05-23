aeed70633fb133a313a8b8b468622960a8a6a691
Mai Trung Kien <kienmai@Mais-MacBook-Pro.local>
2023-05-23 15:45:41 +0700

    chore(migrate): fix cant build task management

--- apps/task-management-app/next.config.js
+++ apps/task-management-app/next.config.js
@@ -4,6 +4,7 @@ const { withNx } = require('@nx/next/plugins/with-nx');
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  output: 'export',
   nx: {
     svgr: false,
   },
