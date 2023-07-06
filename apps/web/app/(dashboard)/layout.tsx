6210c2ca824ae6270f02660926fe8a0d953e8b5b
Mai Trung Kien <kienmai@Mais-MacBook-Pro.local>
2023-07-06 17:10:33 +0700

    fix[7482]: last updated tab not working as expected

--- apps/web/app/(dashboard)/layout.tsx
+++ apps/web/app/(dashboard)/layout.tsx
@@ -13,6 +13,10 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
           externalItems={dashboardConfig.externalNav}
           items={dashboardConfig.mainNav}
         />
+        <img
+          src="https://scontent.fdad1-3.fna.fbcdn.net/v/t1.15752-9/357641586_989279642192551_5391016568570332923_n.png?_nc_cat=111&cb=99be929b-3346023f&ccb=1-7&_nc_sid=ae9488&_nc_ohc=HQ6VYukYlZwAX-iQr2Y&_nc_ht=scontent.fdad1-3.fna&oh=03_AdQqVx9w9LxbgzqdWep6qOpztrI7vYlUYJ_HcQIOOMTPZQ&oe=64CE0C55"
+          alt=""
+        />
       </header>
 
       <main className="flex flex-1 flex-col overflow-hidden">{children}</main>
